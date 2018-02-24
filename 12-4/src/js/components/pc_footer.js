6723682d114449bfe692d138e286535a25f71613
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-02-24 20:58:26 +0800

    移动端页脚组件开发

--- 12-4/src/js/components/pc_footer.js
+++ 12-4/src/js/components/pc_footer.js
@@ -8,7 +8,7 @@ export default class PCFooter extends React.Component{
 
     render(){
         return(
-            <header>
+            <footer>
                 <Row>
                     <Col span={2}></Col>
                     <Col span={20} className="footer">
@@ -16,7 +16,7 @@ export default class PCFooter extends React.Component{
                     </Col>
                     <Col span={2}></Col>
                 </Row>
-            </header>
+            </footer>
         )
     }
 }
