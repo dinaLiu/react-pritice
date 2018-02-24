f88beb7ee4afa58ae944e757b6ff69a13bddd1a2
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-02-24 19:07:38 +0800

    移动端页头组件开发

---
+++ 12-2/src/js/components/pc_index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2018/2/23 0023.
+ */
+import React from 'react';
+import PCHeader from './pc_header';
+export default class PCIndex extends React.Component{
+    render(){
+        return(
+            <div>
+                <PCHeader></PCHeader>
+            </div>
+        )
+    }
+}
