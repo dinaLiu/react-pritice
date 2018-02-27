85e9e13f0a14ee617eae1e8bff3503242f03e25f
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-02-27 18:46:41 +0800

    13-7

---
+++ 14-1/webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Administrator on 2018/1/27 0027.
+ */
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context: __dirname + '/src',
+    entry: "./js/root.js",
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins:['react-html-attrs']  //添加组件的插件配置
+                }
+            },
+            //下面是使用ant-design的配置文件
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+               // test:/\.css/,
+
+                //loader:'style!css-loader?modules&importLoader=1&localIdentName=[name]_[local]__[hash:base64:5]'
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/src/',
+        filename: "bundle.js"
+    },
+    devServer:  {
+        port:8080,
+        historyApiFallback: true
+    },
+
+
+};
