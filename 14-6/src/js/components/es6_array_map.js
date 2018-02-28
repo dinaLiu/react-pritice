cc3be5af4f3adbffa44ed7ae8629376cbfae1034
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-02-28 21:15:31 +0800

    tijiao

--- 14-6/src/js/components/es6_array_map.js
+++ 14-6/src/js/components/es6_array_map.js
@@ -4,16 +4,20 @@ import React from 'react';
 export default class ES6ArrayMap  extends React.Component{
 
     componentWillMount(){
+
         const users = [
             {name: "张含韵", "email": "zhang@email.com"},
             {name: "江一燕",   "email": "jiang@email.com"},
             {name: "李小璐",  "email": "li@email.com"}
         ];
 
+        for(var i = 0;i <users.length;i++){
+            console.log(users[i].email);
+        }
+
         //emails 接收 users 里面所有的邮箱
         var emails = users.map(function (user,index) {
-                              console.log("当前索引:"+index + " 当前索引对应的值" + user);
-
+                              //console.log("当前索引:"+index + " 当前索引对应的值" + user);
                               return user.email;
                     });
 
