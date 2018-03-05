4ab6f953de49226e5c55a9c38b439c31d7ce1cfa
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-03-05 15:19:56 +0800

    no message

--- 19-3/src/js/reducers.js
+++ 19-3/src/js/reducers.js
@@ -1,6 +1,4 @@
-/**
- * Created by Administrator on 2018/3/5 0005.
- */
+
 export default (state = 0,action)=>{ /*默认函数定义state,reducers对action值的接受*/
     state = state || {counter:0}; /*当第一次调用的时候，会做一个初始化 ; 这个实例是对web一个小的计数器进行加减操作*/
     switch(action.type){
