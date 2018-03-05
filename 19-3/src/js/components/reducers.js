04811da74b43eef3f11648448993ec148c933554
刘霞\Administrator <dinaliu1990@gmail.com.cn>
2018-03-05 14:26:32 +0800

    19-3 reducers传入失败

--- 19-3/src/js/components/reducers.js
+++ 19-3/src/js/components/reducers.js
@@ -3,7 +3,7 @@
  */
 export default (state = 0,action)=>{ /*默认函数定义state,reducers对action值的接受*/
     state = state || {counter:0}; /*当第一次调用的时候，会做一个初始化 ; 这个实例是对web一个小的计数器进行加减操作*/
-    switch(action,type){
+    switch(action.type){
         case 'ADD': /*如果传过来的action是加就ADD，就加1*/
             return{counter:state.counter + 1};
 
--- 19-3/src/js/root.js
+++ 19-3/src/js/root.js
@@ -1,8 +1,36 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import reducer from './reducers';
 
 export default class Root extends React.Component{
+    /*定义UI上的action*/
+    inc(){ /*增加的action*/
+        return{type:'ADD'};
+    }
+
+    dec(){ /*减少的action*/
+        return{type:'SUB'};
+    }
+
+    componentDidMount(){
+        //初始化 传递的参数是reducer
+        var store = createStore(reducer);
+
+        console.log(store.getState());
+
+        store.dispatch(this.inc());
+        console.log(store.getState());
+
+        store.dispatch(this.inc());
+        console.log(store.getState());
+
+        store.dispatch(this.dec());
+        console.log(store.getState());
+    }
+
+
     render(){
 
         return(
